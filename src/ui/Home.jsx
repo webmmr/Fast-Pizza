7a59780129df8899dc35d05a3d99e6eaacdcdd17
webmmr <mdmizanur3950@gmail.com>
2023-08-17 00:29:39 +0600

    CSS Bug Fix

--- src/ui/Home.jsx
+++ src/ui/Home.jsx
@@ -1,19 +1,23 @@
 import CreateUser from "../features/user/CreateUser";
 import Pizza from "../assets/pizza.jpg";
+import PizzaMobile from "../assets/pizza-mobile.jpg";
 import { useSelector } from "react-redux";
 import Button from "./Button";
 
 function Home() {
   const username = useSelector((state) => state.user.username);
 
+  const isMobileScreen = window.innerWidth < 768;
+  console.log(Pizza);
+
   return (
     <div className="">
-      <div className="flex  items-center justify-center">
-        <div className="flex-1">
+      <div className="flex flex-wrap items-center justify-center px-5 py-10 lg:py-0 ">
+        <div className="w-full text-center md:w-1/2 md:text-left">
           <h6 className="text-md mb-3 font-semibold">
             The best pizza in town.
           </h6>
-          <h1 className="leading mb-10 text-5xl font-light text-lime-500">
+          <h1 className="leading mb-10 text-3xl font-light text-lime-500 md:text-4xl lg:text-5xl">
             Straight out of the oven, straight to your door.
           </h1>
 
@@ -25,8 +29,12 @@ function Home() {
             </Button>
           )}
         </div>
-        <div className="h-full basis-2/5">
-          <img src={Pizza} alt="Piiza Image" className="rounded-lg shadow-lg" />
+        <div className="mt-5 h-full w-full text-center md:mt-0 md:w-1/2">
+          <img
+            src={`${isMobileScreen ? PizzaMobile : Pizza}`}
+            alt="Piiza Image"
+            className="rounded-lg shadow-lg"
+          />
         </div>
       </div>
     </div>
