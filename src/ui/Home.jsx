cad8c5764f90479acbfe7c9ee1105a39739c4311
Md Mizanur Rahman <118674977+webmmr@users.noreply.github.com>
2023-08-21 12:41:17 +0600

    Update Home.jsx

--- src/ui/Home.jsx
+++ src/ui/Home.jsx
@@ -31,13 +31,9 @@ function Home() {
         <div className="mt-5 h-full w-full text-center md:mt-0 md:w-1/2">
           <picture>
             <source media="(min-width:768px)" srcset={Pizza}>
-            <img src={PizzaMobile} alt="Pizza Image" className="rounded-lg shadow-lg">
+            <img src={PizzaMobile} alt="Pizza Image" className="rounded-lg shadow-lg" />
           </picture>
-          <img
-            src={`${isMobileScreen ? PizzaMobile : Pizza}`}
-            alt="Piiza Image"
-            className="rounded-lg shadow-lg"
-          />
+          
         </div>
       </div>
     </div>
