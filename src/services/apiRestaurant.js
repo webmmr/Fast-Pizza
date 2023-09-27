6a6241b4bf126dcf9439625dceb1a4423ed0926a
Md Mizanur Rahman <118674977+webmmr@users.noreply.github.com>
2023-09-28 02:50:20 +0600

    Update apiRestaurant.js

--- src/services/apiRestaurant.js
+++ src/services/apiRestaurant.js
@@ -47,7 +47,7 @@ export async function updateOrder(id, updateObj) {
     });
 
     if (!res.ok) throw Error();
-    // We don't need the data, so we don't return anything
+   
   } catch (err) {
     throw Error("Failed updating your order");
   }
